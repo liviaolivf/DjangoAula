10c1368404ad335904988bf576b9f30b61c3ac9f
liviaolivf <livof18@gmail.com>
2024-09-12 22:32:21 -0300

    :tada: add some in urls.py

--- projeto/projeto/urls.py
+++ projeto/projeto/urls.py
@@ -15,8 +15,17 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+from django.http import HttpResponse
 from django.urls import path
+
+def my_view(request):
+    return HttpResponse("Um teste string de resposta")
+
+def user_view(request, username):
+    return HttpResponse(f"Perfil do usuário: {username}")
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('sobre/', my_view),
+    path('user/<str:username>/', user_view)
 ]
